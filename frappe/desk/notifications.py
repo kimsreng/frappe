bd2b074c2a0bdda642e33ba02d98640d46585744
kimsreng <kimsreng1@gmail.com>
2021-07-23 15:16:47 +0700

    Take permission into account

--- frappe/desk/notifications.py
+++ frappe/desk/notifications.py
@@ -258,11 +258,11 @@ def get_open_count(doctype, name, items=[]):
 			# get the fieldname for the current document
 			# we only need open documents related to the current document
 			filters[fieldname] = name
-			total = len(frappe.get_all(d, fields="name",
+			total = len(frappe.get_list(d, fields="name",
 				filters=filters, limit=100, distinct=True, ignore_ifnull=True))
 			data["open_count"] = total
 
-		total = len(frappe.get_all(d, fields="name",
+		total = len(frappe.get_list(d, fields="name",
 			filters={fieldname: name}, limit=100, distinct=True, ignore_ifnull=True))
 		data["count"] = total
 		out.append(data)
