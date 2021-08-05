4f6a2b2522631995472e0f88ffeb98b84d228adf
kimsreng <kimsreng1@gmail.com>
2021-08-06 06:01:06 +0700

    Apply before_insert hook before checking "create" permission

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -223,12 +223,15 @@ class Document(BaseDocument):
 
 		self.set("__islocal", True)
 
-		self.check_permission("create")
 		self._set_defaults()
 		self.set_user_and_timestamp()
 		self.set_docstatus()
 		self.check_if_latest()
 		self.run_method("before_insert")
+		#run before insert method first
+		#to allow any value injection
+		#to insure there will be no user permission error
+		self.check_permission("create")
 		self._validate_links()
 		self.set_new_name(set_name=set_name, set_child_names=set_child_names)
 		self.set_parent_in_children()
