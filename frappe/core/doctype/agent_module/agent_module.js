6bbf52ddb4e6069e2e9c333ae101ac82e139d411
kimsreng <kimsreng1@gmail.com>
2021-10-23 20:50:17 +0700

    agent doctype update

--- frappe/core/doctype/agent_module/agent_module.js
+++ frappe/core/doctype/agent_module/agent_module.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Agent Module', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frappe.xcall('frappe.core.doctype.module_def.module_def.get_installed_apps').then(r => {
+			frm.set_df_property('app_name', 'options', JSON.parse(r));
+		});
+	}
 });
