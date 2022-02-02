5abc317a43508407fa8c9f0e1d2365d17bd73ebe
kimsreng <kimsreng1@gmail.com>
2022-02-02 16:01:23 +0700

    Remove agent abbr from link display value

--- frappe/public/js/frappe/form/formatters.js
+++ frappe/public/js/frappe/form/formatters.js
@@ -136,7 +136,7 @@ frappe.form.formatters = {
 					href="/app/${encodeURIComponent(frappe.router.slug(doctype))}/${encodeURIComponent(original_value)}"
 					data-doctype="${doctype}"
 					data-name="${original_value}">
-					${__(options && options.label || value)}</a>`;
+					${__(options && options.label || frappe.remove_abbr(value))}</a>`;
 			} else {
 				return value;
 			}
